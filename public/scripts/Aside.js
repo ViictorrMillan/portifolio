daa670e1195250a0a97120534c65396e4c703353
Victor <victorhugomartinsmillan@hotmail.com>
2025-05-23 11:02:12 -0300

    (feat): Commit inicial, Aside  e layout principal

---
+++ public/scripts/Aside.js
@@ -0,0 +1,34 @@
+// Pega os elementos
+const menuToggle = document.getElementById('menu-toggle');
+const aside = document.querySelector('aside');
+const overlay = document.getElementById('overlay');
+const asideT = document.querySelector('.aside-t');
+
+// Função que alterna o menu e o overlay
+function toggleMenu() {
+  aside.classList.toggle('active');
+  asideT.classList.toggle('active'); // Mantém junto com aside
+
+  if (aside.classList.contains('active')) {
+    overlay.style.display = 'block';
+  } else {
+    overlay.style.display = 'none';
+  }
+}
+
+menuToggle.addEventListener('click', toggleMenu);
+
+overlay.addEventListener('click', () => {
+  aside.classList.remove('active');
+  asideT.classList.remove('active'); // Removendo junto
+  overlay.style.display = 'none';
+});
+
+const navLinks = aside.querySelectorAll('a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    aside.classList.remove('active');
+    asideT.classList.remove('active'); // Removendo junto
+    overlay.style.display = 'none';
+  });
+});
